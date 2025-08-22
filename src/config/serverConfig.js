b77769a4f9c26b9a42e0a115f669cf9738a8ccb8
Sai Kiran <kamandlasaikiran@gmail.com>
2025-08-22 20:58:28 +0530

    added nodemailer config and configured nodemailer

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -13,3 +13,7 @@ export const NODE_ENV = process.env.NODE_ENV || 'development';
 export const JWT_SECRET = process.env.JWT_SECRET;
 
 export const JWT_EXPIRY = process.env.JWT_EXPIRY || '1d';
+
+export const MAIL_ID = process.env.MAIL_ID;
+
+export const MAIL_PASSWORD = process.env.MAIL_PASSWORD;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { PORT } from './config/serverConfig.js';
 import { StatusCodes } from 'http-status-codes';
 import connectDB from './config/dBConfig.js';
 import apiRouter from './routes/apiRoutes.js';
-
+// import mailer from './config/mailConfig.js';
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded());
@@ -14,9 +14,16 @@ app.get('/ping', (req, res) => {
   });
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`Server is running on port ${PORT}`);
   connectDB();
+  // const mailResponse = await mailer.sendMail({
+  //   from: 'kamandlasaikiran@gmail.com',
+  //   to: 'kamandlasaikiran@gmail.com',
+  //   subject:'Welcome Mail',
+  //   text: 'Welcome to the application'
+  // });
+  // console.log(mailResponse);
 });
 
 app.use('/api', apiRouter);
